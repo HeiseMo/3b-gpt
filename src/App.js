58b9172ddfe40fbf4f5156c114455c6f03f8b71d
Timur James Tanurhan <timurjames.tanurhan@3bscientific.com>
2023-09-17 15:09:33 +0200

    new layout

--- src/App.js
+++ src/App.js
@@ -1,29 +1,40 @@
+import React, { useState } from 'react';
 import NavBar from './components/NavBar';
 import './App.css';
 import ChatInterface from './components/ChatInterface';
 import { ThemeProvider } from './ThemeContext';
-import { React, useState } from 'react';
-import Footer from './components/Footer'
-import MyButton from './components/MyButton'
+import Footer from './components/Footer';
+import MyButton from './components/MyButton';
 
 function App({ toggleTheme, isDarkMode }) {
-  // Define your sidebar items here
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  // Function to toggle the sidebar
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
 
   return (
     <ThemeProvider>
-      <div className={`App text-foreground bg-background`}>
-        <NavBar toggleTheme={toggleTheme} isDarkMode={isDarkMode} />
-        <div className="main-content">
-        <div className="sidebar shadow">
-          <div className="header"> 
-            <MyButton />
+      <div className={`App text-foreground bg-background flex`}>
+        <div
+          className={`sidebar shadow ${isSidebarOpen ? '' : 'collapsed'}`}
+          style={{ flexShrink: isSidebarOpen ? 1 : 0 }}
+        >
+          <div className="header">
+            <MyButton toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
           </div>
           <div className="content">Chats</div>
           <div className="footer">
             <Footer />
           </div>
         </div>
-          <ChatInterface />
+        <div className={`main-content flex`}>
+          {/* Pass the toggleSidebar function to the NavBar */}
+          <NavBar className="justify-start" toggleTheme={toggleTheme} isDarkMode={isDarkMode} toggleSidebar={toggleSidebar} />
+          <div className="flex-grow">
+            <ChatInterface />
+          </div>
         </div>
       </div>
     </ThemeProvider>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,29 @@
 import React from "react";
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
-import Theme from './Theme'
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Button } from "@nextui-org/react";
+import Theme from './Theme';
+import {SideBarIcon} from '../SVG/SideBarIcon';
 
-export default function NavBar({ toggleTheme ,isDarkMode  }) {
-    return (
-      <Navbar position="static">
+export default function NavBar({ toggleTheme, isDarkMode, toggleSidebar }) {
+  return (
+    <Navbar position="static">
+      <NavbarContent align="start">
+        <Button
+          variant="bordered"
+          onClick={toggleSidebar}
+          className="w-[24px] h-[24px] m-0 p-0"
+          color="default"
+        >
+          <SideBarIcon size={24} />
+        </Button>
         <NavbarBrand>
           <p className="font-bold text-inherit">3B-GPT</p>
         </NavbarBrand>
-        <NavbarContent justify="end">
-          <NavbarItem>
-            <Theme toggleTheme={toggleTheme} isDarkMode={isDarkMode} />
-          </NavbarItem>
-        </NavbarContent>
-      </Navbar>
-    );
-  }
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem>
+          <Theme toggleTheme={toggleTheme} isDarkMode={isDarkMode} />
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
+}
